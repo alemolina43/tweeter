68d0ad3d91f0deee262aa9fe4e95d29d8e351ec1
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-03-12 13:12:03 -0700

    Form submission using Jquery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  console.log('Ready to tweet');
+    
+  $(".create-tweet").on("submit", function(event) {
+    event.preventDefault();
+  
+    const formData = $(this).serialize();
+
+    $.post("/api/tweets", formData, function(response) {
+      console.log(response);
+    });
+  });
+});
