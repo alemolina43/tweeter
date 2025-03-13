651259575b74af12d06e0ab3eb9960688f125f78
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-03-12 20:18:11 -0700

    Implement sanitizeInput to prevent XSS with escaping

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -23,15 +23,6 @@ $(document).ready(function() {
 
   loadTweets();
 
-  const sanitizeInput = (input) => {
-    return input
-      .replace(/&/g, "&amp;")
-      .replace(/</g, "&lt;")
-      .replace(/>/g, "&gt;")
-      .replace(/"/g, "&quot;")
-      .replace(/'/g, "&#039;");
-  };
-    
   $(".create-tweet").on("submit", function(event) {
     event.preventDefault();
     let tweetText = $("textarea").val().trim();
@@ -43,21 +34,15 @@ $(document).ready(function() {
       return alert("Tweet should be 140 characters or less");
 
     }
-    // Sanitize the text before sending
-    tweetText = sanitizeInput(tweetText);
-    $("textarea").val(tweetText); //Update the textarea value with sanitized text
-
-
+   
     const formData = $(this).serialize();
-    // $.post("/api/tweets", formData, function() {
-    //   console.log('data sent', formData);
-    // });
-
+  
     $.ajax({
       type: "POST",
       url: "/api/tweets",
       data: formData,
       success: (tweet) => {
+        $("#tweets-container").empty();
         loadTweets();
       }
     });
@@ -74,15 +59,21 @@ const appendTweet = function(tweet) {
 };
 
 const renderTweets = function(tweetsArr) {
-  $("#tweets-container").empty();
   //loop through the array and create HTML article for each
   for (let tweet of tweetsArr) {
     appendTweet(tweet);
   }
 };
 
+const sanitizeInput = function(str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
 const createTweetElement = function(tweetObj) {
   const time = timeago.format(tweetObj['created_at']);
+  const textFromUser = tweetObj.content.text;
 
   let $tweet = `
   <article class="tweet">
@@ -93,7 +84,7 @@ const createTweetElement = function(tweetObj) {
       </div>
       <span class="handle">${tweetObj.user.handle}</span>
     </header>
-    <p>${tweetObj.content.text}</p>
+    <p>${sanitizeInput(textFromUser)}</p>
     <footer>
       <span class="timestamp">${time}</span>
       <div class="icons">
